e89cad9bc2d2c4a0e882c5f115676db219b6d680
palakgoel30 <gpalak7055@gmail.com>
2023-08-24 15:28:07 +0530

    giving title to gui

--- Tkinter.py
+++ Tkinter.py
@@ -1,4 +1,5 @@
 from tkinter import * #Step1
 
 main_window = Tk() #Step2 assinging tk object to main window variable
+main_window.title("Lets Starts Tkinter")
 main_window.mainloop() #Step4 creates a mainloop
