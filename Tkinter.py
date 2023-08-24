3164943bf90b68f6c8908b3ddd25495209185cc2
palakgoel30 <gpalak7055@gmail.com>
2023-08-24 15:33:33 +0530

    initializing the gui window size

--- Tkinter.py
+++ Tkinter.py
@@ -1,5 +1,7 @@
 from tkinter import * #Step1
 
 main_window = Tk() #Step2 assinging tk object to main window variable
-main_window.title("Lets Starts Tkinter")
+main_window.title("Lets Starts Tkinter") # adding title
+main_window.minsize(width = 400, height= 200) # initailize the minimum size of gui window
+main_window.maxsize(width=800,height=500) # initalize the maximum size of gui window
 main_window.mainloop() #Step4 creates a mainloop
