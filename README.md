53df42fe8f634972dc37b03f69c2e8d615e44be8
palakgoel30 <gpalak7055@gmail.com>
2023-08-27 17:03:57 +0530

    implementing new widget i.e, Label and apply attributes to it

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # Python_Tkinter
 Steps to create GUI using Tkinter:
 
-Step 1: import tkinter module
+Step 1: import tkinter module</br>
 Step 2: Creating a main window
 Step 3: Adding multiple widgets in a window
 Step 4: Entering into main event Loop to perform action.
 
 Widgets:- Widgets is a element of gui that displays information.
 Mainloop:- basically it is an infinite loop that runs application ,wait for a event, and processes it as soon as window is open. 
+<H1> WIDGETS</H1>
+Label : Add text,images, Apply different types of attributes.
+
+<H1> Geometry configuration of widgets</H1>
+pack() : it is used to place a widget on top.
+grid() : it is used to organize a widget in table like structure.
+place() : it is used to organize a widget at specific position. 
--- Tkinter.py
+++ Tkinter.py
@@ -1,7 +1,20 @@
-from tkinter import * #Step1
+from tkinter import *  # Step1
 
-main_window = Tk() #Step2 assinging tk object to main window variable
-main_window.title("Lets Starts Tkinter") # adding title
-main_window.minsize(width = 400, height= 200) # initailize the minimum size of gui window
-main_window.maxsize(width=800,height=500) # initalize the maximum size of gui window
-main_window.mainloop() #Step4 creates a mainloop
+main_window = Tk()  # Step2 assinging tk object to main window variable
+main_window.title("Lets Starts Tkinter")  # Adding title
+main_window.minsize(width=400, height=200)  # Initialize the minimum size of gui window
+main_window.maxsize(width=800, height=500)  # Initialize the maximum size of gui window
+
+# Different types of Widgets
+# LABEL
+l1 = Label(main_window, text="Music Music !!",bg = 'Black',fg = 'white')
+l1.pack()
+#l1.grid(row=8, column=5)  # Use grid for l1 label
+#l1.place(x=50,y =70)
+image1= PhotoImage(file="C:/Users/gpala/Desktop/Palak/Code/Python_Tkinter/images/logo.png")
+
+l2 = Label(main_window, image = image1)
+l2.pack()
+#l2.place(x=10, y=70)
+#l2.grid(row=4, column=5)
+main_window.mainloop()  # Step4 creates a main loop
