a127b51ca1dbd5c17858b95dd9f615df63f8661c
palakgoel30 <gpalak7055@gmail.com>
2023-09-13 15:10:16 +0530

    Add Check button

--- Button.py
+++ Button.py
@@ -8,7 +8,11 @@ main_window.maxsize(width=800, height=500)
 # Button
 b1 = Button(main_window, text="Submit")
 b1.place(x=350, y=300)
-
+#check button
+cb1 = Checkbutton(main_window,text = 'Male')
+cb2 = Checkbutton(main_window,text = 'Female')
+cb1.pack()
+cb2.pack()
 # Entry
 b2 = Entry(main_window, width=20, bd=6, font=("calibri",20))
 # b2.grid(row=5,column = 9)
